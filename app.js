f087fb7c9009109d254884e21076dc03776bd987
Maribel <marrodgar30@gmail.com>
2023-12-06 17:22:53 +0100

    chore: Create route get photographer

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(express.json());
 
 app.use('/', router)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`👂 in http://localhost:${PORT}`));

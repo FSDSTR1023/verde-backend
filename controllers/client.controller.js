1a73fa8c44cc7e984b187cdf18a48764fe08efe8
Maribel <marrodgar30@gmail.com>
2024-02-08 15:33:27 +0100

    commit

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,12 +1,10 @@
-import { request, response } from 'express'
-import { ClientModel } from '../models/client.model.js';
-import { clientToObject } from '../helpers/clientToObject.js';
-import { PhotographerModel } from '../models/photographer.model.js';
+import { request, response } from "express";
+import { ClientModel } from "../models/client.model.js";
+import { clientToObject } from "../helpers/clientToObject.js";
+import { PhotographerModel } from "../models/photographer.model.js";
 
 export class Client {
-
   static register = async (req = request, res = response) => {
-
     const photographerId = req.photographerId;
 
     const { name, surname, email, phone } = req.body;
@@ -14,41 +12,33 @@ export class Client {
     if (!name || !surname || !email || !phone) {
       res.status(400).json({
         ok: false,
-        msg: 'Es necesario el name, surname, email y phone para crear un cliente',
-      })
+        msg: "Es necesario el name, surname, email y phone para crear un cliente",
+      });
     }
 
     try {
-
       const clientCreated = await ClientModel.create(req.body);
       await clientCreated.save();
 
       const clientResponse = clientToObject(clientCreated);
 
-      await PhotographerModel.findByIdAndUpdate(
-        photographerId,
-        {
-          $push: { clients: clientResponse.id }
-        }
-      );
+      PhotographerModel.findByIdAndUpdate(photographerId, {
+        $push: { clients: clientResponse.id },
+      });
 
       res.status(201).json({
         ok: true,
-        msg: 'Cliente creado con éxito',
-        clientResponse
+        msg: "Cliente creado con éxito",
+        clientResponse,
       });
-
     } catch (error) {
-
       res.status(400).json({
         ok: false,
-        msg: 'no se pudo crear el cliente',
-        error: error.message
-      })
-
+        msg: "no se pudo crear el cliente",
+        error: error.message,
+      });
     }
-
-  }
+  };
 
   // TODO:
   // static getById

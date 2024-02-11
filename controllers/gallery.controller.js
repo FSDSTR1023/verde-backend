e28702ed611424215729d4125f1c9e09cc809e1a
Maribel <marrodgar30@gmail.com>
2024-02-11 16:34:32 +0100

    Cambios

--- controllers/gallery.controller.js
+++ controllers/gallery.controller.js
@@ -1,175 +1,147 @@
-import { request, response } from 'express';
-import { PhotographerServices } from '../services/photographer.services.js';
-import { galleryModel } from '../models/gallery.model.js';
-import { ClientModel } from '../models/client.model.js';
-import { galleryToObject } from '../helpers/galleryToObject.js';
-import { PhotographerModel } from '../models/photographer.model.js';
-import { photographerToObject } from '../helpers/photographerToObject.js';
+import { request, response } from "express";
+import { PhotographerServices } from "../services/photographer.services.js";
+import { galleryModel } from "../models/gallery.model.js";
+import { ClientModel } from "../models/client.model.js";
+import { galleryToObject } from "../helpers/galleryToObject.js";
+import { PhotographerModel } from "../models/photographer.model.js";
+import { photographerToObject } from "../helpers/photographerToObject.js";
 
 export class GalleryContoller {
+  static create = async (req = request, res = response) => {
+    const photographerId = req.photographerId;
 
+    const { title, client, photos, minPics, totalPrice } = req.body;
 
-    static create = async (req = request, res = response) => {
+    try {
+      const [photographer, clientInfo] = await Promise.all([
+        PhotographerServices.findById(photographerId),
+        ClientModel.findById(client),
+      ]);
 
-        const photographerId = req.photographerId;
+      if (!clientInfo || !photographer) {
+        throw Error("Este cliente o fotógrafo no existe en la base de datos");
+      }
 
-        const { title, client, photos, minPics, totalPrice } = req.body;
+      const galleryData = {
+        title,
+        client,
+        photos,
+        minPics,
+        totalPrice,
+      };
 
-        try {
+      const newGallery = await galleryModel.create(galleryData);
+      newGallery.save();
 
-            const [photographer, clientInfo] = await Promise.all([
-                PhotographerServices.findById(photographerId),
-                ClientModel.findById(client),
-            ])
+      const galleryObject = galleryToObject(newGallery);
 
-            if (!clientInfo || !photographer) {
-                throw Error('Este cliente o fotógrafo no existe en la base de datos');
-            }
+      await Promise.all([
+        PhotographerModel.findByIdAndUpdate(photographerId, {
+          $push: { gallery: galleryObject.id },
+        }),
+        ClientModel.findByIdAndUpdate(clientInfo.id, {
+          $push: { gallery: galleryObject.id },
+        }),
+      ]);
 
-            const galleryData = {
-                title,
-                client,
-                photos,
-                minPics,
-                totalPrice
-            }
+      res.status(201).json({
+        ok: true,
+        msg: "Galería creada",
+        gallery: galleryObject,
+      });
+    } catch (error) {
+      res.status(400).json({
+        ok: false,
+        msg: "hubo un error al crear la galería",
+        error,
+      });
+    }
+  };
 
-            const newGallery = await galleryModel.create(galleryData);
-            newGallery.save();
+  static getAll = async (req = request, res = response) => {
+    const photographerId = req.photographerId;
 
-            const galleryObject = galleryToObject(newGallery);
+    try {
+      const photographer = await PhotographerModel.findById(photographerId)
+        .populate("gallery")
+        .exec();
+      const photograperResponse = photographerToObject(photographer);
 
-            await Promise.all([
-                PhotographerModel.findByIdAndUpdate(photographerId, {
-                    $push: { gallery: galleryObject.id }
-                }),
-                ClientModel.findByIdAndUpdate(clientInfo.id, {
-                    $push: { gallery: galleryObject.id }
-                }),
-            ]);
+      res.status(200).json({
+        ok: true,
+        msg: "Tus galerías son",
+        galeries: photograperResponse.gallery,
+      });
+    } catch (error) {
+      console.trace(error);
 
-            res.status(201).json({
-                ok: true,
-                msg: 'Galería creada',
-                gallery: galleryObject
-            })
+      res.status(400).json({
+        ok: true,
+        msg: "Algo salió mal",
+        error,
+      });
+    }
+  };
 
-        } catch (error) {
-            res.status(400).json({
-                ok: false,
-                msg: "hubo un error al crear la galería"
-            })
-        }
+  static getById = async (req = request, res = response) => {
+    const id = req.params.id;
 
+    if (!id) {
+      res.status(404).json({
+        ok: true,
+        msg: "No se ha proporcionado el ID de la galería",
+      });
     }
 
-    static getAll = async (req = request, res = response) => {
+    try {
+      const gallery = await galleryModel.findById(id).populate("client").exec();
 
-        const photographerId = req.photographerId;
+      const galleryResponse = galleryToObject(gallery);
 
-        try {
+      res.status(200).json({
+        ok: true,
+        msg: `Galería con id: ${id}`,
+        gallery: galleryResponse,
+      });
+    } catch (error) {
+      console.trace(error);
 
-            const photographer = await PhotographerModel.findById(photographerId).populate('gallery').exec();
-            const photograperResponse = photographerToObject(photographer);
+      res.status(400).json({
+        ok: true,
+        msg: "Algo salió mal",
+        error,
+      });
+    }
+  };
 
-            res.status(200).json({
-                ok: true,
-                msg: 'Tus galerías son',
-                galeries: photograperResponse.gallery
-            })
+  static getByIdClient = async (req = request, res = response) => {
+    const id = req.params.id;
 
-
-
-        } catch (error) {
-
-            console.trace(error);
-
-            res.status(400).json({
-                ok: true,
-                msg: 'Algo salió mal',
-                error
-            });
-        }
-
+    if (!id) {
+      res.status(404).json({
+        ok: true,
+        msg: "No se ha proporcionado el ID de la galería",
+      });
     }
 
+    try {
+      const gallery = await galleryModel.findById(id);
 
-    static getById = async (req = request, res = response) => {
+      const galleryResponse = galleryToObject(gallery);
 
-        const id = req.params.id
+      res.status(200).json({
+        ok: true,
+        msg: `Galería con id: ${id}`,
+        gallery: galleryResponse,
+      });
+    } catch (error) {
+      console.trace(error);
 
-        if (!id) {
-            res.status(404).json({
-                ok: true,
-                msg: 'No se ha proporcionado el ID de la galería',
-            })
-
-        }
-
-        try {
-
-            const gallery = await galleryModel.findById(id).populate('client').exec();
-
-            const galleryResponse = galleryToObject(gallery);
-
-            res.status(200).json({
-                ok: true,
-                msg: `Galería con id: ${id}`,
-                gallery: galleryResponse
-            })
-
-
-
-        } catch (error) {
-
-            console.trace(error);
-
-            res.status(400).json({
-                ok: true,
-                msg: 'Algo salió mal',
-                error
-            });
-        }
-
+      res.status(400).json({
+        ok: false,
+        msg: "Algo salió mal",
+        error,
+      });
     }
-
-    static getByIdClient = async (req = request, res = response) => {
-
-        const id = req.params.id
-
-        if (!id) {
-            res.status(404).json({
-                ok: true,
-                msg: 'No se ha proporcionado el ID de la galería',
-            })
-
-        }
-
-        try {
-
-            const gallery = await galleryModel.findById(id);
-
-            const galleryResponse = galleryToObject(gallery);
-
-            res.status(200).json({
-                ok: true,
-                msg: `Galería con id: ${id}`,
-                gallery: galleryResponse
-            })
-
-
-
-        } catch (error) {
-
-            console.trace(error);
-
-            res.status(400).json({
-                ok: false,
-                msg: 'Algo salió mal',
-                error
-            });
-        }
-
-    }
-
+  };
 }
